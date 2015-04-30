6ae706f8e3e6e101cf9a3191f10e2f7e1cec3076
phitchcock <phitchcock@me.com>
2015-04-30 09:08:14 -0700

    setting up reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,29 @@
 class ReviewsController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = current_user.reviews.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit!
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430155847) do
+ActiveRecord::Schema.define(version: 20150430160245) do
 
   create_table "establishments", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150430155847) do
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
